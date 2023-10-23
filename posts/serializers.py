1bde382a34e53052ea1d316d462f4363340ddaf5
BWNC-code <bryn.wheeler@gmail.com>
2023-10-23 20:55:41 +0000

    refactor posts serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,50 +2,45 @@ from rest_framework import serializers
 from .models import Post
 from likes.models import Like
 
+MAX_IMAGE_SIZE_MB = 2
+MAX_DIMENSION = 4096
 
 class PostSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source="owner.username")
+    owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source="owner.profile.id")
-    profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
-    comments_count = serializers.ReadOnlyField()
-    likes_count = serializers.ReadOnlyField()
-
-    def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
-            raise serializers.ValidationError("Image size greater than 2MB!")
-        if value.image.width > 4096:
-            raise serializers.ValidationError("Image width greater than 4096px")
-        if value.image.height > 4096:
-            raise serializers.ValidationError("Image height greater than 4096px")
-        return value
-
-    def get_is_owner(self, obj):
-        request = self.context["request"]
-        return request.user == obj.owner
-
-    def get_like_id(self, obj):
-        user = self.context["request"].user
-        if user.is_authenticated:
-            like = Like.objects.filter(owner=user, post=obj).first()
-            return like.id if like else None
-        return None
 
     class Meta:
         model = Post
         fields = [
-            "id",
-            "owner",
-            "created_at",
-            "updated_at",
-            "title",
-            "content",
-            "image",
-            "is_owner",
-            "profile_id",
-            "profile_image",
-            "like_id",
-            "comments_count",
-            "likes_count",
+            'id', 'owner', 'created_at', 'updated_at', 'title', 'content',
+            'image', 'is_owner', 'profile_id', 'profile_image', 'like_id',
+            'comments_count', 'likes_count',
         ]
+
+    def validate_image(self, value):
+        errors = []
+
+        if value.size > 1024 * 1024 * MAX_IMAGE_SIZE_MB:
+            errors.append(f"Image size greater than {MAX_IMAGE_SIZE_MB}MB!")
+        if value.image.width > MAX_DIMENSION:
+            errors.append(f"Image width greater than {MAX_DIMENSION}px")
+        if value.image.height > MAX_DIMENSION:
+            errors.append(f"Image height greater than {MAX_DIMENSION}px")
+        
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return value
+
+    def get_is_owner(self, obj):
+        return self.context['request'].user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like_id = Like.objects.filter(owner=user, post=obj).values_list('id', flat=True).first()
+            return like_id
+        return None
