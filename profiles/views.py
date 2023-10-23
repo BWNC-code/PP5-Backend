a63d22c82c0bcd8d28183e1bedcd663cbd1675ca
BWNC-code <bryn.wheeler@gmail.com>
2023-10-23 21:04:17 +0000

    improve function naming and fix comments

--- profiles/views.py
+++ profiles/views.py
@@ -6,7 +6,7 @@ from .models import Profile
 from .serializers import ProfileSerializer
 
 
-def get_annotated_profiles():
+def get_profile_stats():
     return Profile.objects.annotate(
         posts_count=Count("owner__post", distinct=True),
         followers_count=Count("owner__followed", distinct=True),
@@ -20,15 +20,15 @@ class ProfileList(generics.ListAPIView):
     No create view as profile creation is handled by django signals.
     """
 
-    queryset = get_annotated_profiles().order_by("-created_at")
+    queryset = get_profile_stats().order_by("-created_at")
     serializer_class = ProfileSerializer
     filter_backends = [
         filters.OrderingFilter,
         DjangoFilterBackend,
     ]
     filterset_fields = [
-        'owner__follower__followed__profile',  # Number of followers the user has
-        'owner__followed__owner__profile'      # Number of people the user follows
+        'owner__follower__followed__profile',  # Profiles that follow the user
+        'owner__followed__owner__profile'      # Profiles the user follows
     ]
     ordering_fields = [
         "posts_count",
@@ -45,5 +45,5 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
 
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = get_annotated_profiles()
+    queryset = get_profile_stats()
     serializer_class = ProfileSerializer
