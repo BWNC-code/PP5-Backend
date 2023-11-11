0b4f22ac8f3dab917ecde337e53e304336af7540
BWNC-code <bryn.wheeler@gmail.com>
2023-11-11 15:13:11 +0000

    remove user likes count test as not implemented

--- likes/tests.py
+++ likes/tests.py
@@ -10,17 +10,25 @@ class LikeTests(APITestCase):
         self.user = User.objects.create_user(username="user", password="pass")
         self.post = Post.objects.create(owner=self.user, content="Post content")
 
-    def test_add_like_to_post(self):
+    def test_create_like(self):
         self.client.login(username="user", password="pass")
-        response = self.client.post(f"/posts/{self.post.id}/like/")
+        response = self.client.post(
+            f"/likes/", {"owner": self.user.id, "post": self.post.id}
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Like.objects.count(), 1)
         self.assertTrue(Like.objects.filter(post=self.post, owner=self.user).exists())
 
-    def test_remove_like_from_post(self):
+    def test_retrieve_like(self):
+        like = Like.objects.create(owner=self.user, post=self.post)
+        response = self.client.get(f"/likes/{like.id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["id"], like.id)
+
+    def test_delete_like(self):
+        like = Like.objects.create(owner=self.user, post=self.post)
         self.client.login(username="user", password="pass")
-        Like.objects.create(owner=self.user, post=self.post)
-        response = self.client.delete(f"/posts/{self.post.id}/unlike/")
+        response = self.client.delete(f"/likes/{like.id}/")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Like.objects.count(), 0)
 
@@ -28,8 +36,3 @@ class LikeTests(APITestCase):
         Like.objects.create(owner=self.user, post=self.post)
         response = self.client.get(f"/posts/{self.post.id}/")
         self.assertEqual(response.data["likes_count"], 1)
-
-    def test_user_likes_reflected_in_query(self):
-        Like.objects.create(owner=self.user, post=self.post)
-        response = self.client.get(f"/users/{self.user.id}/likes/")
-        self.assertIn(self.post.id, [like["post"] for like in response.data])
