d33d26daecf9b391f2d6c7fdb61b9f875fb5ee03
BWNC-code <bryn.wheeler@gmail.com>
2023-11-11 15:05:11 +0000

    add tests for likes

--- likes/tests.py
+++ likes/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from posts.models import Post
+from .models import Like
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class LikeTests(APITestCase):
+    def setUp(self):
+        user = User.objects.create_user(username="user", password="pass")
+        post = Post.objects.create(owner=user, content="Post content")
+        Like.objects.create(owner=user, post=post)
+
+    def test_like_post(self):
+        self.assertEqual(Like.objects.count(), 1)
+        like = Like.objects.first()
+        self.assertIsNotNone(like)
+
+    def test_retrieve_like(self):
+        like = Like.objects.first()
+        response = self.client.get(f"/likes/{like.id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
