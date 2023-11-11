4a635d897c13fee7c538aae712b6e9708464bd4e
BWNC-code <bryn.wheeler@gmail.com>
2023-11-11 14:53:15 +0000

    create test scripts for profiles and posts

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpass123"
+        )
+        self.post = Post.objects.create(
+            owner=self.user, title="Test Post", content="Just a test post"
+        )
+
+    def test_post_creation(self):
+        self.client.login(username="testuser", password="testpass123")
+        data = {"title": "New Post", "content": "Content of the new post"}
+        response = self.client.post("/posts/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count(), 2)
+        self.assertEqual(Post.objects.latest("id").title, "New Post")
+
+    def test_post_list(self):
+        response = self.client.get("/posts/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        results = response.data.get("results", [])
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0]["title"], "Test Post")
+
+    def test_post_detail(self):
+        response = self.client.get("/posts/1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["title"], "Test Post")
+        self.assertEqual(response.data["content"], "Just a test post")
+
+    def test_post_update(self):
+        self.client.login(username="testuser", password="testpass123")
+        data = {"title": "Updated Post", "content": "Updated content"}
+        response = self.client.put("/posts/1/", data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.post.refresh_from_db()
+        self.assertEqual(self.post.title, "Updated Post")
+
+    def test_post_delete(self):
+        self.client.login(username="testuser", password="testpass123")
+        response = self.client.delete("/posts/1/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Post.objects.count(), 0)
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class ProfileListViewTests(APITestCase):
+    """
+    Tests for the Profile model list view
+    """
+
+    def setUp(self):
+        testuser1 = User.objects.create_user(username="test1", password="passwordpass")
+        testuser2 = User.objects.create_user(username="test2", password="passwordpass")
+
+    def test_profile_automatically_created_on_user_creation(self):
+        response = self.client.get("/profiles/")
+        count = Profile.objects.count()
+        self.assertEqual(count, 2)
+
+    def test_can_list_profiles(self):
+        response = self.client.get("/profiles/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ProfileDetailViewTests(APITestCase):
+    """
+    Tests for the Profile model detail view
+    """
+
+    def setUp(self):
+        samar = User.objects.create_user(username="test1", password="passwordpass")
+        charlotte = User.objects.create_user(username="test2", password="passwordpass")
+
+    def test_cant_retrieve_profile_using_invalid_id(self):
+        response = self.client.get("/profiles/999/")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_can_retrieve_profile_using_valid_id(self):
+        response = self.client.get("/profiles/2/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
