0edae278fdd79917ab9223404bfeebe01a7b7f3e
BWNC-code <bryn.wheeler@gmail.com>
2023-11-11 15:03:08 +0000

    add tests to edit and delete comments

--- comments/tests.py
+++ comments/tests.py
@@ -4,20 +4,34 @@ from .models import Comment
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-
 class CommentTests(APITestCase):
     def setUp(self):
-        user = User.objects.create_user(username="user", password="pass")
-        post = Post.objects.create(owner=user, content="Post content")
-        Comment.objects.create(owner=user, post=post, content="Comment content")
+        self.user = User.objects.create_user(username="user", password="pass")
+        self.post = Post.objects.create(owner=self.user, content="Post content")
+        self.comment = Comment.objects.create(owner=self.user, post=self.post, content="Comment content")
 
     def test_create_comment(self):
         self.assertEqual(Comment.objects.count(), 1)
-        comment = Comment.objects.first()
-        self.assertEqual(comment.content, "Comment content")
+        self.assertEqual(self.comment.content, "Comment content")
 
     def test_retrieve_comment(self):
-        comment = Comment.objects.first()
-        response = self.client.get(f"/comments/{comment.id}/")
+        response = self.client.get(f"/comments/{self.comment.id}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["content"], "Comment content")
+
+    # logs in as the user, updates the comment content, and verifies that the update was successful
+    def test_edit_comment(self):
+        self.client.login(username="user", password="pass")
+        updated_content = "Updated Comment Content"
+        response = self.client.put(f"/comments/{self.comment.id}/", {"content": updated_content})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.comment.refresh_from_db()
+        self.assertEqual(self.comment.content, updated_content)
+
+    # logs in as the user, deletes the comment, and verifies that the comment was
+    # successfully removed from the database
+    def test_delete_comment(self):
+        self.client.login(username="user", password="pass")
+        response = self.client.delete(f"/comments/{self.comment.id}/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Comment.objects.count(), 0)
