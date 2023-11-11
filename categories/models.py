3d235bb6b1ae159be198394a1eb18e5a220e71a4
BWNC-code <bryn.wheeler@gmail.com>
2023-11-11 11:08:33 +0000

    add description to category model

--- categories/models.py
+++ categories/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Category(models.Model):
+    """
+    Category model, related to Post
+    """
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
 
