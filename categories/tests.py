0611ccddb517080bb95fdcb03c07eea97cd15ac0
BWNC-code <bryn.wheeler@gmail.com>
2023-11-11 15:14:44 +0000

    add tests for categories

--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from .models import Category
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class CategoryTests(APITestCase):
+    def setUp(self):
+        Category.objects.create(name="Adventure", description="Adventure related posts")
+
+    def test_create_category(self):
+        self.assertEqual(Category.objects.count(), 1)
+        category = Category.objects.first()
+        self.assertEqual(category.name, "Adventure")
+
+    def test_retrieve_category(self):
+        category = Category.objects.first()
+        response = self.client.get(f"/categories/{category.id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["name"], "Adventure")
